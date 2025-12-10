8c701bb5a6b95c49b64570b741569a5894b2f1dc
Carlos Wimmer <coiote777@gmail.com>
2025-12-10 03:34:15 +0100

    Include image and link

--- src/pages/blog/posts/index.tsx
+++ src/pages/blog/posts/index.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Posts() {
   return (
-    <div>
-      <h2>Posts</h2>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
+      <h2 className="text-3xl font-bold text-gray-100 mb-8">Posts</h2>
+      <Image src="/assets/field.jpeg" alt="Field" width={600} height={400} />
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,12 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
-      <h2 className="text-3xl font-bold text-gray-100">Hello World</h2>
+      <h2 className="text-3xl font-bold text-gray-100 mb-8">Hello World</h2>
+      <div className="text-blue-500">
+        <Link href="/blog">Go to blog</Link>
+        {" | "}
+        <Link href="/about">Go to about</Link>
+      </div>
     </div>
   );
 }
