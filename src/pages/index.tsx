1bd47d01d931697df331217dc9994d71080440d3
Carlos Wimmer <coiote777@gmail.com>
2025-12-10 02:19:45 +0100

    Downgrade Tailwind CSS configuration and styles: replaced JavaScript config with TypeScript, removed old config file, and updated global styles to use Tailwind directives.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,8 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-      <h2>Hello World</h2>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
+      <h2 className="text-3xl font-bold text-gray-100">Hello World</h2>
     </div>
   );
 }
