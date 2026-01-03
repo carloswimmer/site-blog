57ee07813c70588012c42ce962b7ebbc5950eb28
Carlos Wimmer <coiote777@gmail.com>
2026-01-03 13:05:50 +0100

    Set default image metadata for layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Site.set",
   description: "Sell your products as an affiliate in one place",
+  openGraph: {
+    title: "Site.set",
+    description: "Sell your products as an affiliate in one place",
+    url: "https://site-blog-kappa.vercel.app/og-image.jpg",
+    siteName: "Site.set",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-kappa.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.set",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
