01e254877b0ff9545d3f7909a5270f44da230b44
Carlos Wimmer <coiote777@gmail.com>
2026-01-03 13:35:43 +0100

    Add SEO in blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { PostPage } from "@/templates/blog";
 
@@ -7,6 +8,40 @@ type BlogPostPageProps = {
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return {
+    title: `${post.title} - Site.set`,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      title: `${post.title} - Site.set`,
+      description: post.description,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/${post.slug}`,
+      siteName: "Site.set",
+      locale: "en_US",
+      type: "website",
+      images: [
+        {
+          url: post.image,
+          width: 800,
+          height: 600,
+          alt: post.title,
+        },
+      ],
+    },
+  };
+}
 
 export const revalidate = 60;
 
