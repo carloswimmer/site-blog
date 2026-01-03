d08207ab7eeebbdb5da70629fc7a6f25254f6bc6
Carlos Wimmer <coiote777@gmail.com>
2026-01-03 12:39:25 +0100

    Include metadata to improve SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
+import type { Metadata } from "next";
 import { LandingPage } from "@/templates/landing-page";
+
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Sell ​​your products as an affiliate in one place",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.set",
+    description: "Sell ​​your products as an affiliate in one place",
+    url: "https://site-blog-kappa.vercel.app/og-image.jpg",
+    siteName: "Site.set",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-kappa.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.set",
+      },
+    ],
+  },
+};
 
 export default function HomePage() {
   return <LandingPage />;
